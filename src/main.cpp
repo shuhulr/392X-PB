b7a8657ffbafa04b011b775a8d1fce9857fa3553
Shuhul Raina <coolshuhul@gmail.com>
2025-10-10 19:03:56 -0700

    added logo display, so we be cool

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
+#include "liblvgl/core/lv_obj_pos.h"
+#include "liblvgl/display/lv_display.h"
+#include "liblvgl/llemu.hpp"
+#include "liblvgl/misc/lv_area.h"
+#include "liblvgl/misc/lv_types.h"
+#include "liblvgl/widgets/image/lv_image.h"
 #include "pros/adi.hpp"
 #include "pros/misc.h"
 #include "pros/rtos.hpp"
@@ -111,6 +117,22 @@ void rightScreenButton() {
     autonIndex = (autonIndex + 1) % (autons.size());
 }
 
+// Aura.
+inline void black_background() { lv_obj_set_style_bg_color( lv_screen_active(), lv_color_hex(0x000000), 0); }
+void displayImage() {
+    pros::lcd::shutdown();
+    black_background();
+    // var for c array
+    LV_IMAGE_DECLARE(speedzappers_logo_rotated);
+    //declare and define the image object
+    lv_obj_t *img = lv_image_create(lv_screen_active());
+
+    // set the source data for the image
+    lv_image_set_src(img, &speedzappers_logo_rotated);
+
+    // alignment
+    lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
+}
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -127,6 +149,9 @@ void initialize() {
 
     
     chassis.calibrate(); // calibrate sensors
+
+    
+    
 
     // the default rate is 50. however, if you need to change the rate, you
     // can do the following.
@@ -164,6 +189,8 @@ void initialize() {
     });
 }
 
+
+
 /**
  * Runs while the robot is disabled
  */
@@ -179,7 +206,6 @@ void disabled() {
 
 
 void competition_initialize() {
-
 }
 
 // get a path used for pure pursuit
@@ -192,31 +218,6 @@ void competition_initialize() {
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    // // Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
-    // chassis.moveToPose(20, 15, 90, 4000);
-    // // Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
-    // chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
-    // // cancel the movement after it has traveled 10 inches
-    // chassis.waitUntil(10);
-    // chassis.cancelMotion();
-    // // Turn to face the point x:45, y:-45. Timeout set to 1000
-    // // dont turn faster than 60 (out of a maximum of 127)
-    // chassis.turnToPoint(45, -45, 1000, {.maxSpeed = 60});
-    // // Turn to face a direction of 90ยบ. Timeout set to 1000
-    // // will always be faster than 100 (out of a maximum of 127)
-    // // also force it to turn clockwise, the long way around
-    // chassis.turnToHeading(90, 1000, {.direction = AngularDirection::CW_CLOCKWISE, .minSpeed = 100});
-    // // Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // // following the path with the back of the robot (forwards = false)
-    // // see line 116 to see how to define a path
-    // chassis.follow(example_txt, 15, 4000, false);
-    // // wait until the chassis has traveled 10 inches. Otherwise the code directly after
-    // // the movement will run immediately
-    // // Unless its another movement, in which case it will wait
-    // chassis.waitUntil(10);
-    // pros::lcd::print(4, "Traveled 10 inches during pure pursuit!");
-    // // wait until the movement is done
-
     //autonIndex = 0; // Change this to whichever auton you want to run
     std::get<1>(autons[autonIndex])();
     
@@ -226,8 +227,11 @@ void autonomous() {
  * Runs in driver control
  */
 void opcontrol() {
+    
+    displayImage();
     // controller
     // loop to continuously update motors
+
     while (true) {
         // get joystick positions
         int leftX = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
@@ -306,64 +310,6 @@ void opcontrol() {
             lowFunnel.toggle();
         }
 
-        
-
-
-        /*// Long goal
-        if (controller.get_digital(DIGITAL_R2)) {
-            fly.move(128);
-            indexer.move(128);
-            intake.move(128);
-        }
-
-        // Mid goal
-        else if (controller.get_digital(DIGITAL_L2)) {
-            indexer.move(128);
-            intake.move(128);
-        }
-
-        // Low goal
-        if (controller.get_digital(DIGITAL_Y)) {
-            indexer.move(128);
-            intake.move(-64);
-        }
-
-        //fly it in incase cooked
-        if (controller.get_digital(DIGITAL_UP)) {
-            fly.move(128);
-        }
-
-        
-
-        // blocker
-        if (controller.get_digital_new_press(DIGITAL_R1)) {
-            blocker.retract();
-        }
-
-        if (controller.get_digital_new_press(DIGITAL_R2)) blocker.extend();
-
-        // instigator
-        if (controller.get_digital_new_press(DIGITAL_B)) instigator.extend();
-
-        if (controller.get_digital_new_release(DIGITAL_B)) instigator.retract();
-
-        // matchloader
-        if (controller.get_digital_new_press(DIGITAL_L2)) matchloader.extend();
-
-        // outdexer
-        if (controller.get_digital_new_press(DIGITAL_DOWN)) indexer.move(-128);
-
-        if (controller.get_digital_new_press(DIGITAL_RIGHT)) matchloader.toggle();
-*/
-
-        // if no intake or indexer or fly buttons are pressed, stop the motors
-
-        /*if (!controller.get_digital(DIGITAL_R1) && !controller.get_digital(DIGITAL_L1)) {
-            intake.move(0);
-            
-        }   
-        if (!controller.get_digital(DIGITAL_L2) && !controller.get_digital(DIGITAL_RIGHT)) intake.move(0);*/
-        // delay to save resources
         pros::delay(10);
     }
 }
